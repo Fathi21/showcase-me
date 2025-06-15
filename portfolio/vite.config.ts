338b1d80efbe2d6e5fd42a76bc49dd4c084a569a
Sharif <48218028+Fathi21@users.noreply.github.com>
2025-06-15 13:21:14 +0000

    feat: update package.json with deployment scripts and set base path in vite.config.ts

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/showcase-me/", // 👈 replace with your repo name
   server: {
     host: "::",
     port: 8080,
